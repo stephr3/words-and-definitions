148cd00f1472424ddc0e408dc93fcedef7757a1b
Stephanie Gurung <stephr3@gmail.com>
2016-07-08 09:02:11 -0700

    pass word spec saves the word in an array

--- lib/word.rb
+++ lib/word.rb
@@ -10,4 +10,8 @@ class Word
     @definitions = []
     @id = @@words.length().+(1)
   end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,11 +3,17 @@ require('word')
 require('definition')
 
 describe(Word) do
-
   describe('#word') do
     it('returns the word') do
       test_word = Word.new({:word => 'Cat'})
       expect(test_word.word()).to(eq('Cat'))
+    end
+  end
+
+  describe('#save') do
+    it('saves the word in an array') do
+      test_word = Word.new({:word => 'Cat'})
+      expect(test_word.save()).to(eq([test_word]))      
     end
   end
 end
