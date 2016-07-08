c9756ae71ba08816fab9658998a9966741e4db4b
Stephanie Gurung <stephr3@gmail.com>
2016-07-08 10:13:37 -0700

    initialize integration spec add word path, a list of added words is displayed for the user

--- spec/words_and_definitions_integration_spec.rb
+++ spec/words_and_definitions_integration_spec.rb
@@ -7,9 +7,17 @@ describe('the add word path', {:type => :feature}) do
   it('allows user to add a word and confirm the word has been added') do
     visit('/')
     click_link('Add a Word')
-    expect(page).to have_content('Add a New Word')
     fill_in('name', :with => 'Cat')
     click_button('Add Word')
+    expect(page).to have_content('Cat')
+  end
+
+  it('allows the user to view a list of words that the user has added') do
+    visit('/')
+    click_link('Add a Word')
+    fill_in('name', :with => 'Cat')
+    click_button('Add Word')
+    click_link('Home')
     expect(page).to have_content('Cat')
   end
 end
