2d53906e2f70165883bf2e3a87dee38795eb613e
Stephanie Gurung <stephr3@gmail.com>
2016-07-08 08:28:13 -0700

    pass all specs to return given definition attributes

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,4 +8,18 @@ describe(Definition) do
       expect(test_definition.part_of_speech()).to(eq('Noun'))
     end
   end
+
+  describe('#definition') do
+    it('returns the definition') do
+      test_definition = Definition.new({:part_of_speech => 'Noun', :definition => 'An animal with four legs, whiskers, and a tail.', :example_sentence => 'The cat meowed by the door all night.'})
+      expect(test_definition.definition()).to(eq('An animal with four legs, whiskers, and a tail.'))
+    end
+  end
+
+  describe('#example_sentence') do
+    it('returns the example_sentence') do
+      test_definition = Definition.new({:part_of_speech => 'Noun', :definition => 'An animal with four legs, whiskers, and a tail.', :example_sentence => 'The cat meowed by the door all night.'})
+      expect(test_definition.example_sentence()).to(eq('The cat meowed by the door all night.'))
+    end
+  end
 end
